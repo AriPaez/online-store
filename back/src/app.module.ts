ac64b233d1739e564a77c33f3ac8b202b789fabc
Ariel Páez <paezariel69@gmail.com>
2025-09-29 15:14:42 -0300

    fix (app.module): resolve  error TS2304: Cannot find name 'PaymentMethodModule'

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,9 +3,15 @@ import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { CustomersModule } from './modules/customers/customers.module';
 import { OrdersModule } from './modules/orders/orders.module';
-import { PaymentMethodModule } from './payment-method/payment-method.module';
+import { PaymentMethodModule } from './modules/payment-method/payment-method.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrdersModule, CustomersModule, PaymentMethodModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    OrdersModule,
+    CustomersModule,
+    PaymentMethodModule,
+  ],
 })
 export class AppModule {}
