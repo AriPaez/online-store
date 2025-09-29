43a8f14bbab6c8043e63017b4034411da8fb329f
Ariel Páez <paezariel69@gmail.com>
2025-09-29 16:33:37 -0300

    feat (AuthService): method declaration

--- back/src/modules/auth/auth.service.ts
+++ back/src/modules/auth/auth.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ChangePasswordUserDto } from './dto/change-password.dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  async verifyToken(token: string) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async registerUser(registerUserDto: RegisterUserDto) {}
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  async updateUser(updateDTO: UpdateUserDto) {}
+  async loginUser(loginUserDto: LoginUserDto) {}
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+  // async findAllUsers(tableDTO: TableDTO) {}
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  async findOneUser(id: string) {}
+
+  async removeUser(id: string) {}
+  async changePassword(changePasswordUserDto: ChangePasswordUserDto) {}
 }
