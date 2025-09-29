28afd440adefa3bc5fafe5c586484c91e61f2774
Ariel Páez <paezariel69@gmail.com>
2025-09-29 07:56:35 -0300

    feat: add module payment-method

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
 import { CustomersModule } from './modules/customers/customers.module';
 import { OrdersModule } from './modules/orders/orders.module';
+import { PaymentMethodModule } from './payment-method/payment-method.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, OrdersModule, CustomersModule],
+  imports: [UsersModule, ProductsModule, OrdersModule, CustomersModule, PaymentMethodModule],
 })
 export class AppModule {}
