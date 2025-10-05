f4a2280690bffe704e8af28679dad75716e4ac28
AriPaez <paezariel69@gmail.com>
2025-10-05 14:56:58 -0300

    feat:  implement method verifyToken customer

--- back/src/modules/customers/customers.controller.ts
+++ back/src/modules/customers/customers.controller.ts
@@ -6,15 +6,24 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { LoginDto } from '../users/dto';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { User } from '../common/decorators/user.decorator';
+import { CurrentUser } from '../common/interfaces/current-user.interface';
+import { Token } from '../common/decorators';
+import { JwtService } from '../common/jwt/jwt.service';
 
 @Controller('customers')
 export class CustomersController {
-  constructor(private readonly customersService: CustomersService) {}
+  constructor(
+    private readonly customersService: CustomersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('register')
   registerCustomer(@Body() createCustomerDto: CreateCustomerDto) {
@@ -24,6 +33,12 @@ export class CustomersController {
   @Post('login')
   loginUser(@Body() loginCustomerDto: LoginDto) {
     return this.customersService.loginCustomer(loginCustomerDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return this.jwtService.verifyToken(token);
   }
 
   @Get()
