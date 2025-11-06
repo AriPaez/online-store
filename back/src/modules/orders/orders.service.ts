69d174de762bc615d1fd6bd110f473b8270c20a5
Karen Pelozo <karenpelozo98@gmail.com>
2025-11-06 16:48:04 -0300

    Agregue funciones de orden y tambien arregle el back de productos

--- back/src/modules/orders/orders.service.ts
+++ back/src/modules/orders/orders.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -51,14 +51,42 @@ export class OrdersService {
     }) ;
   }
   // AGREGO MAS FUNCIONES PARA LAS ORDENES
-  async totalOrden(){}
-  async calcularImpuestos(id_order){}
-  async calcularCostoEnvio(id_order){}
-  async cambiarEstado(id_order, nuevoEstado:Status){}
-  async cancelarOrden(id_order){}
-  async filtrarOrdenesPorFecha(desde, hasta){}
-  async marcarComoPagada(id_order, metodoPago) {}
-  async actualizarCantidad(id_order, id_producto, nuevaCantidad: number){}
-  async ordenesPendientes() {}
-  async ordenesCompletadas(){}
+  async totalOrden(){
+    const ordens = await this.prisma.orders.findMany({
+      include: {
+        products: true
+      }
+    });
+    const totalGeneral = ordens.reduce((acum, orden) => {
+    const totalOrden = orden.products.reduce((sum, prod) => sum + (prod.price ?? 0), 0);
+    return acum + totalOrden;
+  }, 0);
+    return {totalGeneral};
+  }
+  //async calcularImpuestos(id_order){}
+  //async calcularCostoEnvio(id_order){}
+  async cambiarEstado(id_order, nuevoEstado:Status){
+    const orden = await this.prisma.orders.findUnique({
+      where: { id_order },
+    });
+
+    if (!orden) {
+      throw new NotFoundException(`No se encontró la orden con ID ${id_order}`);
+    }
+    const ordenActualizada = await this.prisma.orders.update({
+      where: { id_order },
+      data: { status: nuevoEstado },
+    });
+    return {
+      id: ordenActualizada.id_order,
+      estado: ordenActualizada.status,
+      message: `El estado de la orden se cambió correctamente a ${nuevoEstado}.`,
+    };
+  }
+  //async cancelarOrden(id_order){}
+  //async filtrarOrdenesPorFecha(desde, hasta){}
+  //async marcarComoPagada(id_order, metodoPago) {}
+  //async actualizarCantidad(id_order, id_producto, nuevaCantidad: number){}
+  //async ordenesPendientes() {}
+  //async ordenesCompletadas(){}
 }
--- back/src/modules/products/products.service.ts
+++ back/src/modules/products/products.service.ts
@@ -11,7 +11,7 @@ export class ProductsService{
  
   //CRUD BASICO  \(0 W 0)/
   async create(createProductDto: CreateProductDto) {
-    return await this.prisma.products.create({
+    const prodCreate=  await this.prisma.products.create({
       data:{
         id_producto:createProductDto.id_producto,
         price:createProductDto.price,
@@ -20,32 +20,51 @@ export class ProductsService{
         stock: createProductDto.stock
       }
     })
+    return{
+      id:prodCreate.id_producto,
+       name: prodCreate.description ?? '',
+      description: prodCreate.description ?? '',
+      price: prodCreate.price,
+      category: prodCreate.category,
+      stock: prodCreate.stock
+    }
   }
 
   async findAll() {
     const productos =  await this.prisma.products.findMany();
     return productos.map( pro => {
+      let price_final=pro.price
       if(pro.discount_price && pro.discount_price > 0 ){
-        const precio_descu = pro.price - ((pro.price * pro.discount_price)/100)
+         price_final = pro.price - ((pro.price * pro.discount_price)/100)
+          }
         return{
+          id: pro.id_producto,
+          name: pro.description ?? '',
+          description: pro.description ?? '',
           ...pro,
-          price : precio_descu
+          price: price_final
         }
       }
-      return pro
-    })
+    )
   }
 
   async findOne(id: string) {
-    return await this.prisma.products.findUnique({
+    const producto= await this.prisma.products.findUnique({
       where:{
         id_producto:id
       }
     });
+    if(!producto) return null;
+    return {
+      id: producto.id_producto,
+       name: producto.description ?? '',
+      description: producto.description ?? '',
+      ...producto
+    }
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
-    return await this.prisma.products.update({
+    const prodUpdate=  await this.prisma.products.update({
       where:{
         id_producto:id
       },
@@ -55,33 +74,56 @@ export class ProductsService{
         category:updateProductDto.category
       }
     }) ;
+    return{
+      id:prodUpdate.id_producto,
+       name: prodUpdate.description ?? '',
+      description: prodUpdate.description ?? '',
+      ...prodUpdate
+    }
   }
 
   async remove(id: string) {
-    return await this.prisma.products.delete({
+    const prodDelete= await this.prisma.products.delete({
       where:{
         id_producto:id
       }
     });
+    return{
+      id: prodDelete.id_producto,
+       name: prodDelete.description ?? '',
+      description: prodDelete.description ?? '',
+      ...prodDelete
+    }
   }
   //ACA VOY A PONER FUNCIONES PARA PRODUCTOS (0 W 0)/ agregue stock 
   //ESTO ES PARA EL USUARIO ADMIN
   async aplicarDescuento(id:string , dtoDescuento: AplicarDescuentoDto){
     const producto = await this.findOne(id)
     if (!producto) throw new NotFoundException('Producto no encontrado');
-    return await this.prisma.products.update({
+    const prod_new= await this.prisma.products.update({
       where: { id_producto: id},
       data: {discount_price : dtoDescuento.porcentaje}
     });
+    return {
+       id:prod_new.id_producto,
+        name: prod_new.description ?? '',
+      description: prod_new.description ?? '',
+      price: prod_new.price,
+      category: prod_new.category,
+      stock: prod_new.stock
+    }
   }
   
   async asignarCategoria(id: string ,dtoCategoria : AsignarCategoriaDto){
       const producto = await this.findOne(id)
      if (!producto) throw new NotFoundException('Producto no encontrado');
-     return await this.prisma.products.update({
+     const pCategoria=  await this.prisma.products.update({
       where:{ id_producto : id},
       data:{ category : dtoCategoria.category}
      })
+     return{
+      id:pCategoria.id_producto
+     }
   }
 
   async valorTotalInventario(){
@@ -101,17 +143,30 @@ export class ProductsService{
     if(listaProductos.length ===0){
       throw new NotFoundException(`No se encontraron productos en la categoría "${categoria}"`);
     }
-    return listaProductos;
+    return listaProductos.map(p=>({
+      id:p.id_producto,
+       name: p.description ?? '',
+      description: p.description ?? '',
+      ...p
+    }));
   }
 
   async filtrarPorRangoDePrecioMax(price_max: number){
-      return await this.prisma.products.findMany({
+      const prodPrecioMax= await this.prisma.products.findMany({
         where:{
           price : {
             lte: price_max
           }
         }
       })
+      return prodPrecioMax.map(p =>({
+           id:p.id_producto,
+            name: p.description ?? '',
+           description: p.description ?? '',
+           price: p.price,
+           category: p.category ?? '',
+           stock: p.stock
+      }))
      
   }
 // me gustaria poner nombre del producto y dejar la descripcion para adjetivos del objeto, como remera color tanto...
